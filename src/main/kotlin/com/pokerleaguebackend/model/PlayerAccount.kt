13c4ec773c9c4730367ce4bb8d97e7db9b9a66dd
Your <you@example.com>
2025-07-06 01:33:23 -0500

    \"Refactor: Remove wildcard imports and ignore application.log\"

--- src/main/kotlin/com/pokerleaguebackend/model/PlayerAccount.kt
+++ src/main/kotlin/com/pokerleaguebackend/model/PlayerAccount.kt
@@ -1,6 +1,13 @@
 package com.pokerleaguebackend.model
 
-import jakarta.persistence.*
+import jakarta.persistence.Entity
+import jakarta.persistence.Table
+import jakarta.persistence.Id
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Column
+import jakarta.persistence.OneToMany
+import jakarta.persistence.CascadeType
 import java.util.ArrayList
 
 @Entity
