33f25d6b65a380f8efeff0c5a728eea1b9423928
Your <you@example.com>
2025-07-24 11:00:09 -0500

    Added clarifying comment to bountyPlacedOnPlayerId in GameResult model.

--- src/main/kotlin/com/pokerleaguebackend/model/GameResult.kt
+++ src/main/kotlin/com/pokerleaguebackend/model/GameResult.kt
@@ -29,5 +29,7 @@ data class GameResult(
 
     @ManyToOne
     @JoinColumn(name = "bounty_placed_on_player_id")
+    // Refers to the player who collected a 'kill bounty' by eliminating this player.
+    // Null if no bounty was collected or if this player was not eliminated by another player.
     val bountyPlacedOnPlayer: LeagueMembership?
 )
