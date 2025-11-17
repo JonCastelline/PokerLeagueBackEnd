ecdd1e7e27fa3b81d56bdef68fb2d75cbc11eab6
Your <you@example.com>
2025-11-17 14:18:25 -0600

    fix: Use UTC to store season date

--- src/main/kotlin/com/pokerleaguebackend/payload/request/CreateSeasonRequest.kt
+++ src/main/kotlin/com/pokerleaguebackend/payload/request/CreateSeasonRequest.kt
@@ -1,10 +1,13 @@
 
 package com.pokerleaguebackend.payload.request
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.util.Date
 
 data class CreateSeasonRequest(
     val seasonName: String,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "UTC")
     val startDate: Date,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "UTC")
     val endDate: Date
 )
--- src/main/kotlin/com/pokerleaguebackend/payload/request/UpdateSeasonRequest.kt
+++ src/main/kotlin/com/pokerleaguebackend/payload/request/UpdateSeasonRequest.kt
@@ -1,9 +1,12 @@
 package com.pokerleaguebackend.payload.request
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.util.Date
 
 data class UpdateSeasonRequest(
     val seasonName: String,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "UTC")
     val startDate: Date,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "UTC")
     val endDate: Date
 )
